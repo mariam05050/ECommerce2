d29b28ad07d2e80230a7ba6f12c7231916ae7487
mariam05050 <mariamdiab0505@gmail.com>
2026-09-16 23:24:15 +0300

    Update project and documentation

--- Auth.Services/AuthValidators.cs
+++ Auth.Services/AuthValidators.cs
@@ -8,21 +8,28 @@ public class RegisterValidator : AbstractValidator<RegisterRequest>
     {
         RuleFor(x => x.Name)
             .NotEmpty()
-            .MaximumLength(100);
+            .MaximumLength(50);
 
         RuleFor(x => x.Email)
             .NotEmpty()
             .EmailAddress()
-            .MaximumLength(150);
+            .MaximumLength(50);
 
         RuleFor(x => x.Password)
             .NotEmpty()
+            .WithMessage("Password is required.")
             .MinimumLength(8)
-            .MaximumLength(100)
+            .WithMessage("Password must be at least 8 characters.")
+            .MaximumLength(20)
+            .WithMessage("Password must not exceed 20 characters.")
             .Matches("[A-Z]")
+            .WithMessage("Password must contain at least one uppercase letter.")
             .Matches("[a-z]")
+            .WithMessage("Password must contain at least one lowercase letter.")
             .Matches("[0-9]")
-            .Matches("[^a-zA-Z0-9]");
+            .WithMessage("Password must contain at least one number.")
+            .Matches("[^a-zA-Z0-9]")
+            .WithMessage("Password must contain at least one special character.");
     }
 }
 
@@ -33,7 +40,7 @@ public class LoginValidator : AbstractValidator<LoginRequest>
         RuleFor(x => x.Email)
             .NotEmpty()
             .EmailAddress()
-            .MaximumLength(150);
+            .MaximumLength(50);
 
         RuleFor(x => x.Password)
             .NotEmpty();
